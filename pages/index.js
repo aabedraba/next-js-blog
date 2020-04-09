1f05744bc955cbb395de0181a24b0d93cb765cca
Abdallah <aabedraba@gmail.com>
2020-04-09 21:06:54 +0200

    Clean dynamic URL for posts

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import Link from 'next/link'
 const PostLink = (props) => {
     return (
         <li>
-            <Link href={`/post?title=${props.title}`}>
+            <Link href="/posts/[id]" as={`/posts/${props.id}`}>
                 <a>{props.title}</a>
             </Link>
         </li>
@@ -16,9 +16,9 @@ export default function Index() {
         <Layout>
             <h1>Blog</h1>
             <ul>
-                <PostLink title="First post" />
-                <PostLink title="Second post" />
-                <PostLink title="Third post" />
+                <PostLink title="First post" id="first-post" />
+                <PostLink title="Second post" id="second-post" />
+                <PostLink title="Third post" id="third-post" />
             </ul>
         </Layout>
     );
